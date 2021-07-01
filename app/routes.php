4b72412fc53862eac6b425a9932718d353b1d3e6
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-07-01 16:52:03 +0200

    First commit

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Medoo\Medoo;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -15,6 +16,34 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
+
+
+
+        // Using Medoo namespace.
+
+
+// Connect the database.
+        $database = new Medoo([
+            'type' => 'pgsql',
+            'host' => 'postgresql',
+            'database' => 'scacchieropoli',
+            'username' => 'scacchieropoli',
+            'password' => 'scacchieropoli'
+        ]);
+
+        $database->insert('account', [
+            'user_name' => 'foo',
+            'email' => 'foo@bar.com'
+        ]);
+
+        $data = $database->select('account', [
+            'user_name',
+            'email'
+        ], [
+            'user_id' => 50
+        ]);
+
+
         $response->getBody()->write('Hello world!');
         return $response;
     });
